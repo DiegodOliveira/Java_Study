64bd63dc4f363a73318a4a4b683217f3e0c81798
DiegodOliveira <112182138+DiegodOliveira@users.noreply.github.com>
2023-04-10 09:48:24 -0300

    Add files via upload

---
+++ Interest.java
@@ -0,0 +1,20 @@
+public class Interest {
+    public static void main(String[] args) {
+
+        double amount; // Quantia em depósito ao fim de cada ano.
+        double principal = 1000.0; // Quantidade inicial antes dos juros.
+        double rate = 0.05; // Taxa de juros.
+
+        // Exibe cabeçalhos
+        System.out.printf("%s%20s %n", "Year", "Amount of deposit");
+
+        // Calcula quantidade de depósito para cada dos dez anos.
+        for (int year = 1; year <= 10; year++){
+            // Calcula nova quantidade durante ano especificado.
+            amount = principal * Math.pow(1.0 + rate, year);
+
+            // Exibe o ano e a quantidade.
+            System.out.printf("%4d%,20.2f%n", year, amount);
+        }
+    }
+}
