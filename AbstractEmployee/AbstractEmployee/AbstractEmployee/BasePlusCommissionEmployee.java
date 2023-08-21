7248166cfecf397505237aae61e30369bb46bd4f
Diego de Oliveira Santana Cipriano <112182138+DiegodOliveira@users.noreply.github.com>
2023-08-21 19:41:42 -0300

    Add files via upload

---
+++ AbstractEmployee/AbstractEmployee/AbstractEmployee/BasePlusCommissionEmployee.java
@@ -0,0 +1,34 @@
+package AbstractEmployee;
+
+public class BasePlusCommissionEmployee extends ComissionEmployee{
+    public double baseSalary;
+
+    public BasePlusCommissionEmployee(String firstName, String lastName, String socialSecurityNumber,double comissionRates, double grossSales, double baseSalary){
+        super(firstName, lastName, socialSecurityNumber, comissionRates, grossSales);
+
+        if(baseSalary < 0.0)
+            throw new IllegalArgumentException("Base salary must be > 0.0");
+
+        this.baseSalary = baseSalary;
+    };
+
+    public void setBaseSalary(double baseSalary) {
+        if(baseSalary < 0.0)
+            throw new IllegalArgumentException("Base salary must be > 0.0");
+
+        this.baseSalary = baseSalary;
+    }
+
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+
+    @Override
+    public double earnings(){
+        return getBaseSalary() + super.earnings();
+    }
+
+    @Override
+    public String toString(){
+        return String.format("%s %s; %s: $%,.2f", "base-salaried", super.toString(), "base salary", getBaseSalary());}
+}
---
+++ AbstractEmployee/AbstractEmployee/AbstractEmployee/ComissionEmployee.java
@@ -0,0 +1,58 @@
+package AbstractEmployee;
+
+public class ComissionEmployee extends Employee{
+
+    private double grossSales;
+    private double comissionRate;
+
+    public ComissionEmployee(String firstName, String lastName, String socialSecurityNumber, double grossSales, double comissionRate){
+        super(firstName, lastName, socialSecurityNumber);
+
+        if(comissionRate <= 0.0 || comissionRate >= 1.0)
+            throw new IllegalArgumentException("Commission rate must be > 0.0 and < 1.0");
+
+        if(grossSales < 0.0)
+            throw new IllegalArgumentException("Commision rate must be > 0.0");
+
+        this.comissionRate = comissionRate;
+        this.grossSales = grossSales;
+    }
+
+    public void setComissionRate(double comissionRate) {
+        if(comissionRate <= 0.0 || comissionRate >= 1.0)
+            throw new IllegalArgumentException("Commission rate must be > 0.0 and < 1.0");
+
+        this.comissionRate = comissionRate;
+    }
+
+    public double getComissionRate() {
+        return comissionRate;
+    }
+
+    public void setGrossSales(double grossSales){
+        if(grossSales < 0.0)
+            throw new IllegalArgumentException("Gross sales must be >= 0.0");
+
+        this.grossSales = grossSales;
+    }
+
+    public double getGrossSales() {
+        return grossSales;
+    }
+
+    @Override
+    public double earnings(){
+        return getComissionRate() * getGrossSales();
+    }
+
+    @Override
+    public String toString(){
+        return String.format("%s: %s%n%s: $%,.2f; %s: %,.2f","Commission employee", super.toString(), "Commission rate", getComissionRate(),
+                "GrosssSales", getGrossSales());
+    }
+
+    @Override
+    public double getPaymentAmount(){
+        return comissionRate;
+    }
+}
